97a862474e9c3d263a3d6db09ce5e590b6844c5d
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-12 20:41:24 -0300

    CTD

--- string.py
+++ string.py
@@ -13,4 +13,43 @@ age = 30
 #print('My name is {name} and I am {age}'.format(name = name, age = age))
 
 ## F-String (version 3.6+)
-print(f'Hello, my name is {name} and I am {age}')
+#print(f'Hello, my name is {name} and I am {age}')
+
+# String Methods
+
+s = 'hello world'
+
+## Capitalize string
+print(s.capitalize())
+
+## Make all uppercase
+
+print(s.upper())
+
+## Make all lower
+
+print(s.lower())
+
+## Swap case
+
+print(s.swapcase()) 
+
+## Get length
+
+print(len(s))
+
+## Replace
+
+print(s.replace('world', 'Jeff'))
+
+## Count
+
+sub = 'h'
+print(s.count(sub))
+
+## Starts with
+print(s.startswith('hello'))
+
+## Ends with
+print(s.endswith(sub))
+
