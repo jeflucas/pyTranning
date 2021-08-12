9104b245c0d04d44bd2bddb9bf4571fff12863cd
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-12 20:13:42 -0300

    CTD

---
+++ variables.py
@@ -0,0 +1,14 @@
+# A variable is a container for a value, which can be of various types
+
+'''
+This is a multiline comment
+or docstring (used to define a functions propose)
+can be single os double quotes
+'''
+
+"""
+Variable rules:
+- Variable names are case sensitive (name, Name and NAME are different variables)
+- Must star with a letter or an undesrscore
+- Can have numbers but can not star with a number
+"""
